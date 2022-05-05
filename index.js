6474b410ece539a91a7425202e5b77f669e6a50c
igorsans <igor.santos0@hotmail.com>
2022-05-05 11:45:20 -0300

    ++
    sprites de movimentação implementados

--- index.js
+++ index.js
@@ -47,7 +47,25 @@ const player = new Figther({
     scale: 2.5,
     offset: {
         x: 215,
-        y:180
+        y: 157
+    },
+    sprites: {
+        idle: {
+            imageSrc: './img/samuraiMack/Idle.png',
+            framesMax: 8
+        },
+        run: {
+            imageSrc: './img/samuraiMack/Run.png',
+            framesMax: 8
+        },
+        jump: {
+            imageSrc: './img/samuraiMack/Jump.png',
+            framesMax: 2
+        },
+        fall: {
+            imageSrc: './img/samuraiMack/Fall.png',
+            framesMax: 2
+        }
     }
 })
 
@@ -94,15 +112,25 @@ function animate() {
     background.update()
     shop.update()
     player.update()
-    enemy.update()
+    // enemy.update()
     player.velocity.x = 0
     enemy.velocity.x = 0
 
     //player movement
     if (keys.a.pressed && lastKey === 'a') {
         player.velocity.x = -5
+        player.switchSprite('run')
     } else if (keys.d.pressed && lastKey === 'd') {
         player.velocity.x = 5
+        player.switchSprite('run')
+    } else{
+    player.switchSprite('idle')
+    }
+    if (player.velocity.y < 0){
+        player.switchSprite('jump')
+    } else if (player.velocity.y > 0){
+        player.switchSprite('fall')
+
     }
     //enemy movement
     if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
--- js/classes.js
+++ js/classes.js
@@ -1,5 +1,5 @@
 class Sprite {
-    constructor({ position, imageSrc, scale = 1, framesMax = 1, offset = {x:0, y:0} }) {
+    constructor({ position, imageSrc, scale = 1, framesMax = 1, offset = { x: 0, y: 0 } }) {
         this.position = position
         // this.velocity = velocity
         this.width = 50
@@ -25,20 +25,25 @@ class Sprite {
             this.position.y - this.offset.y,
             (this.image.width / this.framesMax) * this.scale,
             this.image.height * this.scale
-            )
+        )
     }
-    update() {
-        this.draw()
+
+    animateFrames(){
         this.framesElapsed++
 
-        if (this.framesElapsed % this.framesHold === 0){
+        if (this.framesElapsed % this.framesHold === 0) {
 
-        if (this.framesCurrent < this.framesMax -1){
-        this.framesCurrent++
-    } else {
-        this.framesCurrent = 0
+            if (this.framesCurrent < this.framesMax - 1) {
+                this.framesCurrent++
+            } else {
+                this.framesCurrent = 0
+            }
+        }
     }
-}
+
+    update() {
+        this.draw()
+        this.animateFrames()
     }
 
 
@@ -53,7 +58,8 @@ class Figther extends Sprite {
         imageSrc,
         scale = 1,
         framesMax = 1,
-        offset = {x:0, y:0}
+        offset = { x: 0, y: 0 },
+        sprites
     }) {
         super({
             position,
@@ -82,20 +88,32 @@ class Figther extends Sprite {
         this.framesCurrent = 0
         this.framesElapsed = 0
         this.framesHold = 5
+        this.sprites = sprites
+
+        for (const sprite in this.sprites) {
+            sprites[sprite].image = new Image()
+            sprites[sprite].image.src = sprites[sprite].imageSrc
+        }
+
     }
 
     //movimentações
     update() {
         this.draw()
+        this.animateFrames()
+        
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x
         this.attackBox.position.y = this.position.y
 
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
-        if (this.position.y + this.height + this.velocity.y >= canvas.height -95) {
+        // gravity function
+        if (this.position.y + this.height + this.velocity.y >= canvas.height - 95) {
             this.velocity.y = 0
+            this.position.y = 331
         } else this.velocity.y += gravity
+
     }
 
     attack() {
@@ -103,5 +121,37 @@ class Figther extends Sprite {
         setTimeout(() => {
             this.isAttacking = false
         }, 100);
+    }
+    switchSprite(sprite){
+        switch (sprite){
+            case 'idle':
+                if (this.image !== this.sprites.idle.image){
+                this.image = this.sprites.idle.image
+                this.framesMax = player.sprites.idle.framesMax
+                this.framesCurrent = 0
+            }
+            break;
+            case 'run':
+                if(this.image !== this.sprites.run.image) {
+                this.image = player.sprites.run.image
+                this.framesMax = player.sprites.run.framesMax
+                this.framesCurrent = 0
+                }
+            break;
+            case 'jump':
+                if(this.image !== this.sprites.jump.image) {
+                this.image = player.sprites.jump.image
+                this.framesMax = player.sprites.jump.framesMax
+                this.framesCurrent = 0
+            }
+            break
+            case 'fall':
+                if(this.image !== this.sprites.fall.image) {
+                this.image = player.sprites.fall.image
+                this.framesMax = player.sprites.fall.framesMax
+                this.framesCurrent = 0
+            }
+            break
+        }
     }
 }
