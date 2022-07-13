9898ad2469b464c0d0352c45a105acf92f4a3632
igorsans <igor.santos0@hotmail.com>
2022-07-12 23:54:14 -0300

    update background
    atualizando background!

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ const background = new Sprite({
     position: {
         x: 0,
         y: 0
-    }, imageSrc: './img/background.png'
+    }, imageSrc: './img/backgroundnew.png'
 })
 const shop = new Sprite({
     position: {
         x: 600,
         y: 128
-    }, imageSrc: './img/shop.png',
+    }, imageSrc: './img/cat.png',
     scale: 2.75,
     framesMax: 6
 })
